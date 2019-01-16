a327d3c8d7696e0c895338be2cc4900bb52e3d1b
SecurityGoblin <46765403+SecurityGoblin@users.noreply.github.com>
2019-01-16 23:47:40 +0000

    Restructuring

--- windows_printf.py
+++
@@ -1,7 +0,0 @@
-
-#Making windows dll function calls through the use of the ctypes library
-from ctypes import *
-
-msvcrt = cdll.msvcrt
-message_string = "Hello World!\n"
-msvcrt.printf("Testing: %s", message_string)
