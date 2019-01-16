5844874cce8640c31737cd8f8f3a13eaa089aa80
SecurityGoblin <46765403+SecurityGoblin@users.noreply.github.com>
2019-01-16 23:47:09 +0000

    Restructuring

--- linux_printf.py
+++
@@ -1,7 +0,0 @@
-#Making linux function calls through the use of the ctypes library
-
-from ctypes import *
-
-libc = CDLL("libc.so.6")
-message_string = "Hello world!\n"
-libc.printf("Testing: %s", message_string)
